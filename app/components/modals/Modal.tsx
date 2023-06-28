5704d823e1206932d0c06c407e433b0adb2a782d
zadmin88 <zaimon88@gmail.com>
2023-06-28 18:02:16 -0500

    Create Navbar styles

---
+++ app/components/modals/Modal.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { useCallback, useEffect, useState } from 'react';
+import { IoMdClose } from 'react-icons/io';
+
+interface ModalProps {
+  isOpen?: boolean;
+  onClose: () => void;
+  onSubmit: () => void;
+  title?: string;
+  body?: React.ReactElement;
+  footer?: React.ReactElement;
+  actionLabel: string;
+  disabled?: boolean;
+  secondaryAction?: () => void;
+  secondaryActionLabel?: string;
+}
+
+const Modal: React.FC<ModalProps> = ({
+  isOpen,
+  onClose,
+  onSubmit,
+  title,
+  body,
+  actionLabel,
+  footer,
+  disabled,
+  secondaryAction,
+  secondaryActionLabel,
+}) => {
+  return <div>Modal</div>;
+};
+
+export default Modal;
