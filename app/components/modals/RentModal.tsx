a2f14739a1399bc352f17f7819fd64150a5e2ab7
zadmin88 <zaimon88@gmail.com>
2023-06-30 16:10:19 -0500

    Image to listing and Cloudinary conf.

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -14,7 +14,7 @@ import Counter from '../inputs/Counter';
 import CategoryInput from '../inputs/CategoryInput';
 import CountrySelect from '../inputs/CountrySelect';
 import { categories } from '../navbar/Categories';
-// import ImageUpload from '../inputs/ImageUpload';
+import ImageUpload from '../inputs/ImageUpload';
 import Input from '../inputs/Input';
 import Heading from '../Heading';
 
@@ -202,20 +202,20 @@ const RentModal = () => {
     );
   }
 
-  // if (step === STEPS.IMAGES) {
-  //   bodyContent = (
-  //     <div className='flex flex-col gap-8'>
-  //       <Heading
-  //         title='Add a photo of your place'
-  //         subtitle='Show guests what your place looks like!'
-  //       />
-  //       <ImageUpload
-  //         onChange={(value) => setCustomValue('imageSrc', value)}
-  //         value={imageSrc}
-  //       />
-  //     </div>
-  //   );
-  // }
+  if (step === STEPS.IMAGES) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading
+          title='Add a photo of your place'
+          subtitle='Show guests what your place looks like!'
+        />
+        <ImageUpload
+          onChange={(value) => setCustomValue('imageSrc', value)}
+          value={imageSrc}
+        />
+      </div>
+    );
+  }
 
   // if (step === STEPS.DESCRIPTION) {
   //   bodyContent = (
