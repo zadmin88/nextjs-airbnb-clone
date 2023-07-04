6f7e5e7ae55ae65a99c512a546d2ee942ef76500
zadmin88 <zaimon88@gmail.com>
2023-07-03 19:12:30 -0500

    solve dynamic problem

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import ClientOnly from './components/ClientOnly';
 interface HomeProps {
   searchParams: IListingsParams;
 }
+export const dynamic = 'force-dynamic';
 
 const Home = async ({ searchParams }: HomeProps) => {
   const listings = await getListings(searchParams);
